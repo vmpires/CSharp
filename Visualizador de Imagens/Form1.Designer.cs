24e4a01f062a418fb9e71368d9ba70a0877eacf2
Victor Pires <vctprs@gmail.com>
2021-08-03 17:37:13 -0300

    Visualizador de imagens

---
+++ Visualizador de Imagens/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Visualizador_de_Imagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagensListBox = new System.Windows.Forms.ListBox();
+            this.favoritosListBox = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.imagemPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagemPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagensListBox
+            // 
+            this.imagensListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imagensListBox.FormattingEnabled = true;
+            this.imagensListBox.Location = new System.Drawing.Point(12, 25);
+            this.imagensListBox.Name = "imagensListBox";
+            this.imagensListBox.Size = new System.Drawing.Size(387, 95);
+            this.imagensListBox.TabIndex = 0;
+            this.imagensListBox.SelectedIndexChanged += new System.EventHandler(this.imagensListBox_SelectedIndexChanged);
+            // 
+            // favoritosListBox
+            // 
+            this.favoritosListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.favoritosListBox.FormattingEnabled = true;
+            this.favoritosListBox.Location = new System.Drawing.Point(12, 168);
+            this.favoritosListBox.Name = "favoritosListBox";
+            this.favoritosListBox.Size = new System.Drawing.Size(387, 95);
+            this.favoritosListBox.TabIndex = 1;
+            this.favoritosListBox.SelectedIndexChanged += new System.EventHandler(this.favoritosListBox_SelectedIndexChanged);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(108, 126);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(187, 26);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Adicionar às Favoritas";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // imagemPictureBox
+            // 
+            this.imagemPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imagemPictureBox.Location = new System.Drawing.Point(12, 293);
+            this.imagemPictureBox.Name = "imagemPictureBox";
+            this.imagemPictureBox.Size = new System.Drawing.Size(387, 224);
+            this.imagemPictureBox.TabIndex = 3;
+            this.imagemPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Favoritas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Todas as imagens";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 529);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imagemPictureBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.favoritosListBox);
+            this.Controls.Add(this.imagensListBox);
+            this.Name = "Form1";
+            this.Text = "Visualizador de Imagens";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagemPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox imagensListBox;
+        private System.Windows.Forms.ListBox favoritosListBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.PictureBox imagemPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Visualizador de Imagens/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visualizador_de_Imagens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] arquivos = Directory.GetFiles(@"C:\windows\web\wallpaper", "*.jpg", SearchOption.AllDirectories);
+            imagensListBox.Items.AddRange(arquivos);
+        }
+
+        private void imagensListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            imagemPictureBox.ImageLocation = imagensListBox.Text;
+            imagemPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            favoritosListBox.Items.Add(imagensListBox.Text);
+        }
+
+        private void favoritosListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            imagemPictureBox.ImageLocation = favoritosListBox.Text;
+            imagemPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
